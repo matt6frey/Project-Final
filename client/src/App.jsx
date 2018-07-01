0c435af53f99a64641f635f88e1dee689272ae3d
Manali7494 <manalibhattacharyya10@gmail.com>
2018-07-01 17:37:43 -0600

    initialized files

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {  
+
+  render() {
+    
+  return (
+      <div className="App">
+        <h1> Users </h1>
+        <p> List of users </p>
+        <ul>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
