3f8e4479f851375da1526a6d1ad8be922a47a9c8
Manali7494 <manalibhattacharyya10@gmail.com>
2018-07-02 12:13:09 -0600

    added more hardcoded content and links to indv recipes

--- client/src/RecipeList.jsx
+++ client/src/RecipeList.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Recipe from "./Recipe.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import { Link } from "react-router-dom";
+
 
 class RecipeList extends Component {
   constructor(props) {
@@ -25,12 +27,72 @@ class RecipeList extends Component {
               cu. Sit ex unum nemore delicatissimi.
             </p>
             <p className="text-right">
-              <a href="" className="btn btn-primary">
-                <span className="fas fa-utensils" /> Select
-              </a>
+              <Link to="/list/1">
+                <button href="" className="btn btn-primary">
+                  <span className="fas fa-utensils" /> Select
+                </button>
+              </Link>
+            </p>
+          </div>
+          <div className="recipe">
+            <img src="" alt="" className="item-image" />
+            <h3>
+              <a href="">Title</a>
+            </h3>
+            <p className="description">
+              Lorem ipsum dolor sit amet, salutatus vituperata est eu, ut est
+              exerci appareat ponderum. Per in solet deleniti repudiandae, te
+              tibique ancillae disputationi his, quo adhuc delectus deseruisse
+              cu. Sit ex unum nemore delicatissimi.
+            </p>
+            <p className="text-right">
+              <Link to="/list/1">
+                <button href="" className="btn btn-primary">
+                  <span className="fas fa-utensils" /> Select
+                </button>
+              </Link>
+            </p>
+          </div>
+          <div className="recipe">
+            <img src="" alt="" className="item-image" />
+            <h3>
+              <a href="">Title</a>
+            </h3>
+            <p className="description">
+              Lorem ipsum dolor sit amet, salutatus vituperata est eu, ut est
+              exerci appareat ponderum. Per in solet deleniti repudiandae, te
+              tibique ancillae disputationi his, quo adhuc delectus deseruisse
+              cu. Sit ex unum nemore delicatissimi.
+            </p>
+            <p className="text-right">
+              <Link to="/list/1">
+                <button href="" className="btn btn-primary">
+                  <span className="fas fa-utensils" /> Select
+                </button>
+              </Link>
+            </p>
+          </div>
+          <div className="recipe">
+            <img src="" alt="" className="item-image" />
+            <h3>
+              <a href="">Title</a>
+            </h3>
+            <p className="description">
+              Lorem ipsum dolor sit amet, salutatus vituperata est eu, ut est
+              exerci appareat ponderum. Per in solet deleniti repudiandae, te
+              tibique ancillae disputationi his, quo adhuc delectus deseruisse
+              cu. Sit ex unum nemore delicatissimi.
+            </p>
+            <p className="text-right">
+              <Link to="/list/1">
+                <button href="" className="btn btn-primary">
+                  <span className="fas fa-utensils" /> Select
+                </button>
+              </Link>
             </p>
           </div>
         </section>
+        <Link to ="/"> <button> Start Over </button> </Link>
         <Footer />
       </div>
     );
