97745f0399d8317408a66a6625eaef81aa689dac
Manali7494 <manalibhattacharyya10@gmail.com>
2018-07-02 12:11:59 -0600

    Updated footer with links to About route

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 class Footer extends Component {
   render() {
     return (
       <footer>
         <p className="text-right">
-          <a href="http://">About this site</a>
+          <Link to="/about">About this site</Link>
         </p>
       </footer>
     );
