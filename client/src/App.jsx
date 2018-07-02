0a86b164732708c2b16417983db8ca2cf94f5f41
Manali7494 <manalibhattacharyya10@gmail.com>
2018-07-01 20:43:18 -0600

    added route links to app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,31 @@
-import React, {Component} from 'react';
-import Header from './Header.jsx';
-import Capture from './Capture.jsx';
-import Home from './Home.jsx';
-import Recipe from './Recipe.jsx';
-import RecipeList from './RecipeList.jsx';
-import Footer from './Footer.jsx';
+import React, { Component } from "react";
 
+import Capture from "./Capture.jsx";
+import Home from "./Home.jsx";
+import Recipe from "./Recipe.jsx";
+import RecipeList from "./RecipeList.jsx";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div>
-      <Header/>
-      <Capture/>
-      <Home />
-      <RecipeList />
-      <Footer />
-      </div>
+      <React.Fragment>
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+        <Router>
+          <Route path="/capture" component={Capture} />
+        </Router>
+        <Router>
+          <Route exact path="/list" component={RecipeList} />
+        </Router>
+        <Router>
+          <Route path="/list/:id" component={Recipe} />
+        </Router>
+      </React.Fragment>
     );
   }
 }
