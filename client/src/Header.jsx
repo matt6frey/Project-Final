6c4449ccc1bcdf6899bf7eeedf592c10b15b9e1d
matt6frey <matt6frey@gmail.com>
2018-07-01 19:03:41 -0600

    Styling Changes

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import logo from './img/chef-logo.png'; // Tell Webpack this JS file uses this image
 
 class Header extends Component {
     render() {
       return (
-        < header >
+      <header>
+        <div class="logo">
+          <img src={logo} width="75px" height="auto" class="logo"/>
+        </div>
         <h1 className="text-center">Right Recipe</h1>
       </header>
       );
     }
   }
   export default Header;
-          
+
