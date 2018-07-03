34e2c02073f03a3e2abb98e79f6a2354683612ac
Manali7494 <manalibhattacharyya10@gmail.com>
2018-07-03 15:33:14 -0600

    chaged it to dynamic (Without server request) front end

--- client/src/RecipeList.jsx
+++ client/src/RecipeList.jsx
@@ -4,95 +4,58 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import { Link } from "react-router-dom";
 
-
 class RecipeList extends Component {
   constructor(props) {
-    super(props);
+    super();
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          recipeName: "Mac and Cheesy",
+          recipeDescription:
+            "----Delicious--------Delicious--------Delicious--------Delicious--------Delicious--------Delicious--------Delicious--------Delicious---------Delicious---------Delicious---------Delicious---------Delicious---------Delicious---------Delicious---------Delicious--------Delicious--------Delicious--------Delicious--------Delicious----"
+        },
+        {
+          id: 2,
+          recipeName: "Chicken Sandwich",
+          recipeDescription:
+            "----Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing----"
+        }
+      ]
+    };
+  }
+
+  getRecipe() {
+    return this.state.recipes.map(item => {
+      let complete_link = `/list/${item.id}`;
+      return (
+        <div className="recipe">
+          <img src="" alt="" className="item-image" />
+          <h3>
+            <a href="">{item.recipeName}</a>
+          </h3>
+          <p className="description">{item.recipeDescription}</p>
+          <p className="text-right">
+            <Link to={complete_link}>
+              <button href="" className="btn btn-primary">
+                <span className="fas fa-utensils" /> Select
+              </button>
+            </Link>
+          </p>
+        </div>
+      );
+    });
   }
 
   render() {
     return (
       <React.Fragment>
         <Header />
-        <section className="recipe-list">
-          <div className="recipe">
-            <img src="" alt="" className="item-image" />
-            <h3>
-              <a href="">Title</a>
-            </h3>
-            <p className="description">
-              Lorem ipsum dolor sit amet, salutatus vituperata est eu, ut est
-              exerci appareat ponderum. Per in solet deleniti repudiandae, te
-              tibique ancillae disputationi his, quo adhuc delectus deseruisse
-              cu. Sit ex unum nemore delicatissimi.
-            </p>
-            <p className="text-right">
-              <Link to="/list/1">
-                <button href="" className="btn btn-primary">
-                  <span className="fas fa-utensils" /> Select
-                </button>
-              </Link>
-            </p>
-          </div>
-          <div className="recipe">
-            <img src="" alt="" className="item-image" />
-            <h3>
-              <a href="">Title</a>
-            </h3>
-            <p className="description">
-              Lorem ipsum dolor sit amet, salutatus vituperata est eu, ut est
-              exerci appareat ponderum. Per in solet deleniti repudiandae, te
-              tibique ancillae disputationi his, quo adhuc delectus deseruisse
-              cu. Sit ex unum nemore delicatissimi.
-            </p>
-            <p className="text-right">
-              <Link to="/list/1">
-                <button href="" className="btn btn-primary">
-                  <span className="fas fa-utensils" /> Select
-                </button>
-              </Link>
-            </p>
-          </div>
-          <div className="recipe">
-            <img src="" alt="" className="item-image" />
-            <h3>
-              <a href="">Title</a>
-            </h3>
-            <p className="description">
-              Lorem ipsum dolor sit amet, salutatus vituperata est eu, ut est
-              exerci appareat ponderum. Per in solet deleniti repudiandae, te
-              tibique ancillae disputationi his, quo adhuc delectus deseruisse
-              cu. Sit ex unum nemore delicatissimi.
-            </p>
-            <p className="text-right">
-              <Link to="/list/1">
-                <button href="" className="btn btn-primary">
-                  <span className="fas fa-utensils" /> Select
-                </button>
-              </Link>
-            </p>
-          </div>
-          <div className="recipe">
-            <img src="" alt="" className="item-image" />
-            <h3>
-              <a href="">Title</a>
-            </h3>
-            <p className="description">
-              Lorem ipsum dolor sit amet, salutatus vituperata est eu, ut est
-              exerci appareat ponderum. Per in solet deleniti repudiandae, te
-              tibique ancillae disputationi his, quo adhuc delectus deseruisse
-              cu. Sit ex unum nemore delicatissimi.
-            </p>
-            <p className="text-right">
-              <Link to="/list/1">
-                <button href="" className="btn btn-primary">
-                  <span className="fas fa-utensils" /> Select
-                </button>
-              </Link>
-            </p>
-          </div>
-        </section>
-        <Link to ="/"> <button> Start Over </button> </Link>
+        <section className="recipe-list">{this.getRecipe()}</section>
+        <Link to="/">
+          {" "}
+          <button> Start Over </button>{" "}
+        </Link>
         <Footer />
       </React.Fragment>
     );
