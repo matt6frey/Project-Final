a668fa30b8fcfee0a20482d6eeabf9687d3b2c21
Manali7494 <manalibhattacharyya10@gmail.com>
2018-07-03 17:04:17 -0600

    combined app/capture states and methods

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import Capture from "./Capture.jsx";
 import Home from "./Home.jsx";
 import Recipe from "./Recipe.jsx";
@@ -11,14 +11,64 @@ import Ingredient from "./Ingredients.jsx";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      imageURL: "",
+      imageName: ""
+    };
   }
+
+  showImage(event) {
+    const name = event.target.files[0].name;
+    console.log("NAME: ", name);
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.setState({
+        imageURL: reader.result,
+        imageName: name
+      });
+    };
+    console.log(reader.result, this.state.imgURL);
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  submitPic = event => {
+    event.preventDefault();
+
+    const fd = new FormData();
+    fd.append("public_id", this.state.imageName);
+    fd.append("upload_preset", "bkb49fvr");
+    fd.append("file", this.state.imageURL);
+
+    axios
+      .post("https://api.cloudinary.com/v1_1/dybwmffcu/upload", fd)
+      .then(res => {
+        axios
+          .post("/upload", {
+            img: res.data.secure_url
+          })
+          .then(res => {
+            console.log(res);
+          });
+      });
+  };
+
   render() {
     return (
       <div className="app">
         <HashRouter>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/capture" component={Capture} />
+            <Route
+              path="/capture"
+              component={() => (
+                <Capture
+                  showImage={this.showImage.bind(this)}
+                  submitPic={this.submitPic.bind(this)}
+                  imgURL={this.state.imageURL}
+                />
+              )}
+            />
             <Route exact path="/list" component={RecipeList} />
             <Route path="/list/:id" component={Recipe} />
             <Route path="/about" component={About} />
