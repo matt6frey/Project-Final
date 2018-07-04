dcec99d88e6a757da84c4b80dd0919e797695329
Manali7494 <manalibhattacharyya10@gmail.com>
2018-07-03 18:38:12 -0600

    refactored recipe list

--- client/src/RecipeList.jsx
+++ client/src/RecipeList.jsx
@@ -5,29 +5,10 @@ import Footer from "./Footer.jsx";
 import { Link } from "react-router-dom";
 
 class RecipeList extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      recipes: [
-        {
-          id: 1,
-          recipeName: "Mac and Cheesy",
-          recipeDescription:
-            "----Delicious--------Delicious--------Delicious--------Delicious--------Delicious--------Delicious--------Delicious--------Delicious---------Delicious---------Delicious---------Delicious---------Delicious---------Delicious---------Delicious---------Delicious--------Delicious--------Delicious--------Delicious--------Delicious----"
-        },
-        {
-          id: 2,
-          recipeName: "Chicken Sandwich",
-          recipeDescription:
-            "----Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing-------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing--------Amazing----"
-        }
-      ]
-    };
-  }
 
   getRecipe() {
-    return this.state.recipes.map(item => {
-      let complete_link = `/list/${item.id}`;
+    return this.props.recipeList.map(item => {
+      let completeLink = `/list/${item.id}`;
       return (
         <div className="recipe">
           <img src="" alt="" className="item-image" />
@@ -36,7 +17,7 @@ class RecipeList extends Component {
           </h3>
           <p className="description">{item.recipeDescription}</p>
           <p className="text-right">
-            <Link to={complete_link}>
+            <Link to={completeLink}>
               <button href="" className="btn btn-primary">
                 <span className="fas fa-utensils" /> Select
               </button>
