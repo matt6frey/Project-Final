72248416b35b10433ffbb9f58ee487f33f4cfab2
matt6frey <matt6frey@gmail.com>
2018-07-06 21:57:31 -0600

    Added Ingredient type images displayed on Ingredient.jsx

--- client/src/Ingredients.jsx
+++ client/src/Ingredients.jsx
@@ -5,13 +5,28 @@ import { Link } from "react-router-dom";
 
 class Ingredient extends Component {
 
+  getImageType(items) {
+    const foodTypes = {
+      grains: 'https://res.cloudinary.com/dybwmffcu/image/upload/v1530932405/Icons/grains.png',
+      meat: 'https://res.cloudinary.com/dybwmffcu/image/upload/v1530932454/Icons/meat.png',
+      seed: 'https://res.cloudinary.com/dybwmffcu/image/upload/v1530932504/Icons/seed.png',
+      spice: 'https://res.cloudinary.com/dybwmffcu/image/upload/v1530932506/Icons/spice.png',
+      vegetable: 'https://res.cloudinary.com/dybwmffcu/image/upload/v1530932480/Icons/vegetables.png',
+      fruit: 'https://res.cloudinary.com/dybwmffcu/image/upload/v1530932417/Icons/fruit.png'
+    };
+    items.forEach( (item) => {
+      item.img = foodTypes[item.type];
+    })
+    return items;
+  }
   // Get the list of all items for rendering
   getItem() {
-    return this.props.items.map(item => {
+    let items = this.getImageType(this.props.items);
+    return items.map(item => {
       return (
         <div className="item" key={item.name}>
           <img
-            src={item.image}
+            src={item.img}
             alt="Ingredient"
             className="item-image"
             height="75px"
