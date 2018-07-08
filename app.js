1c423d21a3cb73a4f0b8a103eea438081b545b02
matt6frey <matt6frey@gmail.com>
2018-07-07 19:57:31 -0600

    yay

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 // const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
+const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
