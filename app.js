f9fb5d9c11ce40053a60af6010fd8f63ca8b3011
matt6frey <matt6frey@gmail.com>
2018-07-07 19:46:36 -0600

    yay

--- app.js
+++ app.js
@@ -36,10 +36,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/src')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // Evaluate Terms
 function termEvaluator (terms, cb) {
