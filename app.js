d9e52b5845019506efcc9d4906a1f509fefa3df2
matt6frey <matt6frey@gmail.com>
2018-07-07 19:59:17 -0600

    yay

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
