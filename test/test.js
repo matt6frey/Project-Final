b87f48a14e03b7945c3cad14a3c94b877c2aee3f
matt6frey <matt6frey@gmail.com>
2018-07-12 11:25:04 -0600

    Changed favicon, edited seed files to show bell peppers for term 'pepper'

--- test/test.js
+++ test/test.js
@@ -10,19 +10,19 @@ chai.use(chaiAsPromised);
 
 describe('App.js Tests: duplicateArray()', function () {
  it('should return a duplicate array', function () {
-      let array = duplicateArray(['blueberry','apple','banana']);
-        assert.equal(array.length, ['blueberry','apple','banana'].length * 2);
-    });
+    let array = duplicateArray(['blueberry','apple','banana']);
+    assert.equal(array.length, ['blueberry','apple','banana'].length * 2);
+  });
 
  it('should return a duplicate array with the pluralized values made singular, if pluralized values exist', function () {
-      let array = duplicateArray(['blueberry','apple','banana']);
-        assert.equal(array.toString(), ['blueberry','apple','banana', 'blueberry','apple','banana'].toString());
-    });
+    let array = duplicateArray(['blueberry','apple','banana']);
+    assert.equal(array.toString(), ['blueberry','apple','banana', 'blueberry','apple','banana'].toString());
+  });
 
-it('should return "false" if given a data type that isn\'t an array', function () {
-      let array = duplicateArray({id: 01, name: 'object'});
-        assert.equal(array, false);
-    });
+  it('should return "false" if given a data type that isn\'t an array', function () {
+    let array = duplicateArray({id: 01, name: 'object'});
+    assert.equal(array, false);
+  });
 
 
 
@@ -32,8 +32,8 @@ describe('App.js Tests: checkDB()', function () {
 // Tried getting Async tests to run but no dice.
 
 it('should return "false" if given a data type that isn\'t a string', function () {
-      let check = checkDB({id: 01, name: 'object'});
-        assert.equal(check, false);
-    });
+  let check = checkDB({id: 01, name: 'object'});
+  assert.equal(check, false);
+});
 
 });
